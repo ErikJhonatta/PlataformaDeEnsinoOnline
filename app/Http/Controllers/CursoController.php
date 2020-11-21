e4d4a4dc8b04958b99b92421a45cf41974738ac9
ErikJhonatta <erikjhonatta@gmail.com>
2020-11-20 23:27:02 -0300

    Adicionando CRUD Cursos

---
+++ app/Http/Controllers/CursoController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Curso;
+use Illuminate\Http\Request;
+
+class CursoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cursos = Curso::all();
+        return response($cursos,200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $curso = new Curso();
+        $curso->fill($request->all());
+        $curso->save();
+        return response("Success",201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Curso $id)
+    {
+        $id->fill($request->all());
+        $id->save();
+
+        return response("Updated",200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Curso $id)
+    {
+        $id->delete();
+        return response("Deleted",200);
+
+    }
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AlunoController;
+use App\Http\Controllers\CursoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,13 @@ Route::post('/aluno/create',[AlunoController::class, 'store']);
 Route::get('/aluno/list',[AlunoController::class, 'index']);
 Route::put('/aluno/edit/{id}',[AlunoController::class, 'update']);
 Route::delete('/aluno/delete/{id}',[AlunoController::class, 'destroy']);
+
+Route::post('/curso/create',[CursoController::class, 'store']);
+Route::get('/curso/list',[CursoController::class, 'index']);
+Route::put('/curso/edit/{id}',[CursoController::class, 'update']);
+Route::delete('/curso/delete/{id}',[CursoController::class, 'destroy']);
+
+Route::post('/matricula/create',[MatriculaController::class, 'store']);
+Route::get('/matricula/list',[MatriculaController::class, 'index']);
+Route::put('/matricula/edit/{id}',[MatriculaController::class, 'update']);
+Route::delete('/matricula/delete/{id}',[MatriculaController::class, 'destroy']);
